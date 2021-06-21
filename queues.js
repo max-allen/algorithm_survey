6881e8abe27c2ca2d57e3b91a5540e64d8146e11
max-allen <contact@maxallen.io>
2021-06-21 19:13:48 -0400

    reimplements queue with linked list class

--- queues.js
+++ queues.js
@@ -1,8 +1,13 @@
+const LinkedList = require('./linkedlists')
+
 /*
   Supports first in, last out retrieval. Appropriate where order is important.
   put and get operations are called enqueue and dequeue.
 */
 
+
+/*
+// implemented as an array
 class Queue {
   constructor() {
     this.data = []
@@ -18,5 +23,22 @@ class Queue {
     return node
   }
 }
+*/
+
+// implemented as linked list
+class Queue {
+  constructor() {
+    this.data = new LinkedList()
+  }
+
+  enqueue(item) {
+    this.data.addToTail(item)
+  }
+
+  dequeue() {
+    return this.data.removeHead()
+  }
+}
+
 
 module.exports = Queue
